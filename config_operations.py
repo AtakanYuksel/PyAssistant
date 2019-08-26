ccfb3cc7349b562bbf351ecf6d6408d7ee991591
Atakan Y <atakanyuksel1997@gmail.com>
2019-08-26 14:47:37 +0300

    added comments explaning the functions

--- config_operations.py
+++ config_operations.py
@@ -4,19 +4,19 @@ from tkinter import messagebox
 def read_cfg():
     my_dict = {}
     try:
-        config_file = open("config.txt", "r+")
+        config_file = open("config.txt", "r+")  # try to open the file
         for a_line in config_file:
             (key, val) = a_line.split(" : ")
             my_dict[key] = int(val.strip())
         config_file.close()
-    except FileNotFoundError:
+    except FileNotFoundError:   # create/reset the file if it does not exist or corrupt
         open("config.txt", "w")
     return my_dict
 
 
 def save_cfg(my_dict):
     try:
-        config_file = open("config.txt", "w")
+        config_file = open("config.txt", "w")   # save to the file
         for key, val in my_dict.items():
             config_file.write(str(key) + " : " + str(val) + "\n")
         config_file.close()
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def tick(clk):
     clk.after(1000, lambda: tick(clk))
 #
 #
-my_dict = config_operations.read_cfg() #anan
+my_dict = config_operations.read_cfg()
 print(my_dict)
 #
 #
