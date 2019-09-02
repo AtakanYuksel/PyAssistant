83d94fc4b5d12cd9f464b6eeac53e292cf02d48f
Atakan Y <atakanyuksel1997@gmail.com>
2019-09-02 15:57:59 +0300

    fixed location string

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def show_date(date_label):
 
 def show_weather(weather_label):
     try:
-        r = requests.get(
-            "https://api.openweathermap.org/data/2.5/weather?q=Ayd%C4%B1n,TR&APPID=1a605e94b1441e47b27482f8faee924c")
+        r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Aydın,TR&"
+                         "APPID=1a605e94b1441e47b27482f8faee924c")
         data_json = r.json()
         temp = pyt.k2c(data_json["main"]["temp"])
         weather = data_json["weather"][0]["main"]
